368316ebe04c3151a35531ef89b1a1e714f0bbb8
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-05-21 23:04:45 +0300

    food page component

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,37 +1,28 @@
-import { Component, Input } from '@angular/core';
-import { EFood} from '../../../shared/models/interfaces/food';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { Food } from '../../../shared/models/interfaces/food';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { FoodService } from '../../../services/food.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { StarRatingComponent } from '../../partials/star-rating/star-rating.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-food-page',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, StarRatingComponent, RouterLink, CommonModule],
   templateUrl: './food-page.component.html',
   styleUrl: './food-page.component.css',
 })
 export class FoodPageComponent {
-
-  food: EFood = {
-    id: '',
-    name: '',
-    price: 0,
-    tags: [],
-    favorite: false,
-    stars: 0,
-    imageUrl: '',
-    origins: [],
-    cookTime: ''
-  };
+  food!: Food;
   faHeart = faHeart;
-  
+  origins: any;
 
-  constructor (activatedRoute: ActivatedRoute, foodService: FoodService) {
+  constructor(activatedRoute: ActivatedRoute, foodService: FoodService) {
     activatedRoute.params.subscribe((params) => {
-      if(params.id)
-        this.food = foodService.getFoodById(params.id);
-    })
+      if (params.id) this.food = foodService.getFoodById(params.id);
+    });
   }
+
 }
