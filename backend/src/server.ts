f791d7c10053fe7e55c62ad02372dde44304fc7d
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-06-04 15:38:18 +0300

    .env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routers/user.routes";
@@ -7,13 +9,29 @@ import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json());
-dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI!)
-.then(
-    () => { console.log("Connection to mongoDb established") },
-    err => { console.log("Failed to connect to mongodb", err) }
-) 
+
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+  console.error('MONGO_URI is not defined in the environment variables');
+  process.exit(1); // Exit the application if no MongoDB URI is provided
+}
+
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log("Connection to MongoDB established");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    // Here you can add more specific error checks if needed
+    if (err.name === 'MongoNetworkError') {
+      console.error('Network error while trying to connect to MongoDB');
+    } else if (err.message.includes('Authentication failed')) {
+      console.error('Authentication failed. Please check your credentials');
+    } else {
+      console.error('MongoDB connection error:', err.message);
+    }
+  });
 
 app.use(
   cors({
